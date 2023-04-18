2fd13f706778256586e098434716246d0f7c11b8
ajit <ajit@afreespace.com>
2023-04-18 16:04:57 +0530

    seperation of initial time and selected start time

--- test/flutter_time_slot_picker_test.dart
+++ test/flutter_time_slot_picker_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_time_slot_picker/flutter_time_slot_picker.dart';
-
 void main() {
   test('adds one to input values', () {});
 }
